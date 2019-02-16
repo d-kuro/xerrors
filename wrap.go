33d050c406245252e3a34cde3116baab4bb1fdc9
d-kuro <kurosawa7620@gmail.com>
2019-02-16 21:42:31 +0900

    goimports.

--- wrap.go
+++ wrap.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "golang.org/x/xerrors"
+	"fmt"
+
+	"golang.org/x/xerrors"
 )
 
 func main() {
-    baseErr := xerrors.New("base error")
-    wrapErr1 := xerrors.Errorf("wrap error1: %w",baseErr)
-    wrapErr2 := xerrors.Errorf("wrap error2: %w",wrapErr1)
-    fmt.Printf("%+v\n", wrapErr2)
+	baseErr := xerrors.New("base error")
+	wrapErr1 := xerrors.Errorf("wrap error1: %w", baseErr)
+	wrapErr2 := xerrors.Errorf("wrap error2: %w", wrapErr1)
+	fmt.Printf("%+v\n", wrapErr2)
 }
